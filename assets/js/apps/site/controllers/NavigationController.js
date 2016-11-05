434a61a8889313f687c1f42ad577f218cd747ef7
Timo Furrer <tuxtimo@gmail.com>
2016-11-05 22:16:22 +0100

    Remove tabs

--- assets/js/apps/site/controllers/NavigationController.js
+++ assets/js/apps/site/controllers/NavigationController.js
@@ -2,13 +2,13 @@
     var NavigationController = function($location) {
         var self = this;
 
-		self.isActive = function(route) {
-			var currentRoute = $location.path();
+        self.isActive = function(route) {
+            var currentRoute = $location.path();
             if(route === '/' && route !== currentRoute) {
                 return false;
             }
-			return currentRoute.substr(0, route.length) === route
-		};
+            return currentRoute.substr(0, route.length) === route
+        };
     };
 
     module.controller('NavigationController', NavigationController);
